ff33e057263882b21e101b4c3aacfb9ef9f9cbec
thayrov <thayrovg@gmail.com>
2023-12-18 19:00:43 -0600

    💡 [ADD]: Implement image smoother algorithm
    Added the imageSmoother function solution for exercise 661, which applies a 3x3 smoothing filter to a given m x n integer matrix representing a grayscale image. The smoother calculates the average of a cell and its surrounding cells, rounding down the result. The implementation handles edge cases where surrounding cells may not be present by adjusting the count of cells considered for the average. This addition adheres to the constraints that the matrix dimensions are between 1 and 200, and the grayscale values are within the range of 0 to 255. Example inputs and their expected outputs have been included in the comments as part of the documentation.

--- problems/easy.ts
+++ problems/easy.ts
@@ -38,3 +38,77 @@ function maxProductDifference(nums: number[]): number {
   let d = nums[1];
   return a * b - c * d;
 }
+
+/* 
+661. Image Smoother
+
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+Given an m x n integer matrix img representing the grayscale of an image, return the image after applying the smoother on each cell of it.
+
+Example 1:
+
+Input: img = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[0,0,0],[0,0,0],[0,0,0]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+
+Example 2:
+
+Input: img = [[100,200,100],[200,50,200],[100,200,100]]
+Output: [[137,141,137],[141,138,141],[137,141,137]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+
+Constraints:
+
+m == img.length
+n == img[i].length
+1 <= m, n <= 200
+0 <= img[i][j] <= 255
+
+</>Code:
+*/
+
+// Define the function 'imageSmoother' with 'img' as a parameter of type number[][].
+function imageSmoother(img: number[][]): number[][] {
+  // Get the number of rows 'm' and columns 'n' from the image.
+  const m = img.length;
+  const n = img[0].length;
+
+  // Initialize 'newImg' as a two-dimensional array filled with zeros.
+  const newImg: number[][] = Array.from({length: m}, () => new Array(n).fill(0));
+
+  // Iterate over each row of the image.
+  for (let i = 0; i < m; i++) {
+    // Iterate over each column of the image.
+    for (let j = 0; j < n; j++) {
+      // Initialize 'sum' to accumulate the values of the neighboring pixels.
+      let sum = 0;
+      // Initialize 'count' to count the number of neighboring pixels considered.
+      let count = 0;
+
+      // Iterate over the neighbors of the current pixel in the 3x3 grid.
+      for (let x = i - 1; x <= i + 1; x++) {
+        for (let y = j - 1; y <= j + 1; y++) {
+          // Check if the neighbor is within the bounds of the image.
+          if (x >= 0 && x < m && y >= 0 && y < n) {
+            // Add the value of the neighbor to 'sum'.
+            sum += img[x][y];
+            // Increment 'count' for each valid neighbor.
+            count++;
+          }
+        }
+      }
+      // Calculate the average value by dividing 'sum' by 'count' and use 'Math.floor' to round down.
+      newImg[i][j] = Math.floor(sum / count);
+    }
+  }
+
+  // Return the new image matrix after smoothing.
+  return newImg;
+}
--- problems/medium.ts
+++ problems/medium.ts
@@ -36,35 +36,39 @@ class ListNode {
   val: number;
   next: ListNode | null;
   constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+    this.val = val === undefined ? 0 : val; // Constructor: Initializes the value of the node. Defaults to 0 if not provided.
+    this.next = next === undefined ? null : next; // Constructor: Initializes the next node. Defaults to null if not provided.
   }
 }
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  let dummyHead = new ListNode(0);
-  let curr = dummyHead;
-  let carry = 0;
+  let dummyHead = new ListNode(0); // Create a dummy head node for the resulting linked list. This helps in simplifying list operations.
+  let curr = dummyHead; // Initialize a current pointer to the dummy head. This will be used to build the result list.
 
+  let carry = 0; // Initialize the carry to 0. This will store any carry-over during addition.
+
+  // Start a loop that runs until both l1 and l2 are fully traversed
   while (l1 !== null || l2 !== null) {
-    let sum = carry;
+    let sum = carry; // Start with the carry value in the sum for this digit.
+
     if (l1 !== null) {
-      sum += l1.val;
-      l1 = l1.next;
+      sum += l1.val; // Add the value of the current node of l1 to sum if l1 is not null.
+      l1 = l1.next; // Move to the next node in l1.
     }
     if (l2 !== null) {
-      sum += l2.val;
-      l2 = l2.next;
+      sum += l2.val; // Add the value of the current node of l2 to sum if l2 is not null.
+      l2 = l2.next; // Move to the next node in l2.
     }
 
-    carry = Math.floor(sum / 10);
-    curr.next = new ListNode(sum % 10);
-    curr = curr.next;
+    carry = Math.floor(sum / 10); // Calculate the carry for the next digit (1 if sum is 10 or more, otherwise 0).
+    curr.next = new ListNode(sum % 10); // Create a new node with the digit part of the sum and add it to the result list.
+    curr = curr.next; // Move the current pointer to the new node.
   }
 
+  // After processing both lists, if there's still a carry, add a new node with the carry value.
   if (carry > 0) {
     curr.next = new ListNode(carry);
   }
 
-  return dummyHead.next;
+  return dummyHead.next; // Return the next node of dummy head, which is the start of the actual result list.
 }
