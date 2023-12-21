b545d08744f4750ef3073de95074095ea85a113d
thayrov <thayrovg@gmail.com>
2023-12-20 23:08:25 -0600

    💡 [ADD]: Implement maxWidthOfVerticalArea Function
    Added a new function  to solve the problem '1637. Widest Vertical Area Between Two Points Containing No Points'. This function takes an array of points as input and returns the maximum width of the vertical area between two points where no points are inside the area. The implementation involves sorting the x-coordinates of the points and then finding the largest gap between consecutive x-coordinates. The function is efficient and fits well within the given problem constraints.

--- problems/medium.ts
+++ problems/medium.ts
@@ -72,3 +72,56 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
 
   return dummyHead.next; // Return the next node of dummy head, which is the start of the actual result list.
 }
+
+/* 1637. Widest Vertical Area Between Two Points Containing No Points
+Medium
+Topics
+Companies
+Hint
+Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
+
+A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
+
+Note that points on the edge of a vertical area are not considered included in the area.
+
+Example 1:
+
+​
+Input: points = [[8,7],[9,9],[7,4],[9,7]]
+Output: 1
+Explanation: Both the red and the blue area are optimal.
+Example 2:
+
+Input: points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
+Output: 3
+
+Constraints:
+
+n == points.length
+2 <= n <= 105
+points[i].length == 2
+0 <= xi, yi <= 109 
+
+</>Code:
+*/
+
+function maxWidthOfVerticalArea(points: number[][]): number {
+  // Extract x-coordinates from the array of points and sort them in ascending order.
+  // map(point => point[0]): Extracts the x-coordinate (first element of each sub-array).
+  // sort((a, b) => a - b): Sorts the x-coordinates in ascending numerical order.
+  let xCoords = points.map(point => point[0]).sort((a, b) => a - b);
+
+  // Initialize the variable to store the maximum width of the vertical area.
+  let maxGap = 0;
+
+  // Iterate through the sorted x-coordinates starting from the second element.
+  for (let i = 1; i < xCoords.length; i++) {
+    // Calculate the gap between the current and previous x-coordinate.
+    // Math.max(maxGap, xCoords[i] - xCoords[i - 1]): Compares the current gap with the maximum gap found so far,
+    // and updates maxGap with the larger value.
+    maxGap = Math.max(maxGap, xCoords[i] - xCoords[i - 1]);
+  }
+
+  // Return the largest gap found, which is the widest vertical area.
+  return maxGap;
+}
