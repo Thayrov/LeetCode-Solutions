ac7cba36518180d228815c2acff6ae59e2f3d8db
thayrov <thayrovg@gmail.com>
2023-12-21 19:16:59 -0600

    💡 [ADD]: Implement Max Score Calculation Function
    Implements the problem 1422,  function to calculate the maximum score after splitting a binary string. The function iterates through the string, tracking the number of '0's in the left substring and the number of '1's in the right substring. It uses this information to find the optimal split point that provides the maximum score, which is the sum of zeros on the left and ones on the right after the split. The function has been tested with various inputs to ensure accuracy and performance within the specified constraints.

--- problems/medium.ts
+++ problems/medium.ts
@@ -74,10 +74,12 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
 }
 
 /* 1637. Widest Vertical Area Between Two Points Containing No Points
+
 Medium
 Topics
 Companies
 Hint
+
 Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
 
 A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
@@ -86,10 +88,10 @@ Note that points on the edge of a vertical area are not considered included in t
 
 Example 1:
 
-​
-Input: points = [[8,7],[9,9],[7,4],[9,7]]
+​Input: points = [[8,7],[9,9],[7,4],[9,7]]
 Output: 1
 Explanation: Both the red and the blue area are optimal.
+
 Example 2:
 
 Input: points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
