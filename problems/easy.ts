53acb4d62b1c9097c2b53ab9208db2edf0403aef
thayrov <thayrovg@gmail.com>
2023-12-24 18:30:23 -0600

    💡 [ADD]: Implement Decoding and Alternating String Functions
    This commit introduces two new problems resolutions in TypeScript: numDecodings and minOperations (Exercises 91 and 1758). The numDecodings function is designed to count the number of ways a numeric string can be decoded into alphabetic characters, based on the mapping 'A' -> '1', 'B' -> '2', ..., 'Z' -> '26'. It uses dynamic programming to efficiently compute the number of decoding ways for each substring. The minOperations function calculates the minimum number of operations required to convert a binary string into an alternating binary string ('0101...' or '1010...'). It achieves this by counting the mismatches with both alternating patterns and selecting the minimum count. These additions enhance the utility of the codebase in dealing with string manipulation and decoding tasks.

--- problems/easy.ts
+++ problems/easy.ts
@@ -326,3 +326,73 @@ function isPathCrossing(path: string): boolean {
   // If no coordinates have been visited more than once, return false
   return false;
 }
+
+/* 1758. Minimum Changes To Make Alternating Binary String
+Easy
+Topics
+Companies
+Hint
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+
+Example 2:
+
+Input: s = "10"
+Output: 0
+Explanation: s is already alternating.
+
+Example 3:
+
+Input: s = "1111"
+Output: 2
+Explanation: You need two operations to reach "0101" or "1010".
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] is either '0' or '1'.
+
+</> Typescript Code:
+*/
+
+function minOperations(s: string): number {
+  // Initialize two counters to track mismatches with the two possible alternating patterns
+  let count1 = 0; // Count mismatches for pattern '0101...'
+  let count2 = 0; // Count mismatches for pattern '1010...'
+
+  // Loop through each character of the string
+  for (let i = 0; i < s.length; i++) {
+    // Check if the current index is even
+    if (i % 2 === 0) {
+      // If the character at the even index is '1', it mismatches with '0101...' pattern
+      if (s[i] === '1') {
+        count1++; // Increment count1 for pattern '0101...'
+      } else {
+        // Otherwise, it mismatches with '1010...' pattern
+        count2++; // Increment count2 for pattern '1010...'
+      }
+    } else {
+      // If the current index is odd
+      // If the character at the odd index is '0', it mismatches with '0101...' pattern
+      if (s[i] === '0') {
+        count1++; // Increment count1 for pattern '0101...'
+      } else {
+        // Otherwise, it mismatches with '1010...' pattern
+        count2++; // Increment count2 for pattern '1010...'
+      }
+    }
+  }
+
+  // Finally, return the minimum of the two mismatch counts
+  // This represents the minimum number of changes needed to make the string alternating
+  return Math.min(count1, count2);
+}
--- problems/medium.ts
+++ problems/medium.ts
@@ -127,3 +127,81 @@ function maxWidthOfVerticalArea(points: number[][]): number {
   // Return the largest gap found, which is the widest vertical area.
   return maxGap;
 }
+
+/* 
+91. Decode Ways
+
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+"AAJF" with the grouping (1 1 10 6)
+"KJF" with the grouping (11 10 6)
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+Given a string s containing only digits, return the number of ways to decode it.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+
+
+Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+Example 3:
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+
+
+Constraints:
+
+1 <= s.length <= 100
+s contains only digits and may contain leading zero(s).
+
+*/
+
+function numDecodings(s: string): number {
+  // If the string is empty or starts with '0', there are no valid decodings.
+  if (s.length === 0 || s[0] === '0') {
+    return 0;
+  }
+
+  // dp array to store the number of ways to decode up to the ith character
+  let dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1; // Base case: one way to decode an empty string
+  dp[1] = s[0] !== '0' ? 1 : 0; // There is one way if the first character is not '0', otherwise 0 ways
+
+  // Iterate over the string starting from the second character
+  for (let i = 2; i <= s.length; i++) {
+    // Check if the single digit is valid (not '0')
+    if (s[i - 1] !== '0') {
+      // If valid, the number of ways to decode it is the same as the previous character
+      dp[i] += dp[i - 1];
+    }
+
+    // Check if the two digits form a valid number between 10 and 26
+    const twoDigit = parseInt(s.substring(i - 2, i), 10);
+    if (twoDigit >= 10 && twoDigit <= 26) {
+      // If valid, add the number of ways to decode the string up to two characters before
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  // The total number of ways to decode the entire string is stored in dp[s.length]
+  return dp[s.length];
+}
