99d66efe56606703fbcf07cf2db1dea9b3b6cb60
thayrov <thayrovg@gmail.com>
2024-01-21 21:19:44 -0600

    💡 [ADD]: Implement Problem 645: Find Set Mismatch
    Added findErrorNums function to identify the duplicate and missing numbers in a set. The solution uses XOR to separate numbers into two groups and then finds the mismatch by comparing with the original set.

--- problems/easy.ts
+++ problems/easy.ts
@@ -849,3 +849,70 @@ function climbStairs(n: number): number {
   // Return the number of ways to climb to the nth step
   return second;
 }
+
+/* 
+645. Set Mismatch
+
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+
+
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+1 <= nums[i] <= 104
+
+</> Typescript Code:
+*/
+
+function findErrorNums(nums: number[]): number[] {
+  let xor = 0,
+    xor0 = 0,
+    xor1 = 0;
+
+  // XOR all the elements in the array
+  for (const num of nums) {
+    xor ^= num;
+  }
+
+  // XOR all numbers from 1 to n
+  for (let i = 1; i <= nums.length; i++) {
+    xor ^= i;
+  }
+
+  // Get the rightmost set bit in xor
+  let rightmostbit = xor & ~(xor - 1);
+
+  // Divide the numbers into two groups and XOR them separately
+  for (const num of nums) {
+    if ((num & rightmostbit) !== 0) xor1 ^= num;
+    else xor0 ^= num;
+  }
+
+  // Do the same for numbers from 1 to n
+  for (let i = 1; i <= nums.length; i++) {
+    if ((i & rightmostbit) !== 0) xor1 ^= i;
+    else xor0 ^= i;
+  }
+
+  // Determine which one is the duplicate and which one is missing
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === xor1) return [xor1, xor0];
+  }
+  return [xor0, xor1];
+}
