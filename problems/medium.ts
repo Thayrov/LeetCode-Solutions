896ef4e4382b01c89089836d11df9da05dbab38e
Thayrov <thayrovg@gmail.com>
2024-06-06 22:20:07 -0600

    💡 [ADD]: 648. Replace Words
    Implemented a function to replace derivatives in a sentence with their corresponding roots from a given dictionary. The solution iterates through each word in the sentence, checking for the shortest prefix present in the dictionary.

--- problems/medium.ts
+++ problems/medium.ts
@@ -5816,3 +5816,58 @@ function isNStraightHand(hand: number[], groupSize: number): boolean {
   // Return true if all cards can be grouped as required
   return true;
 }
+
+/* 
+648. Replace Words
+
+In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+
+Return the sentence after the replacement.
+
+Example 1:
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+
+Example 2:
+Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+Output: "a a b c"
+
+Constraints:
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] consists of only lower-case letters.
+1 <= sentence.length <= 106
+sentence consists of only lower-case letters and spaces.
+The number of words in sentence is in the range [1, 1000]
+The length of each word in sentence is in the range [1, 1000]
+Every two consecutive words in sentence will be separated by exactly one space.
+sentence does not have leading or trailing spaces.
+
+</> Typescript Code:
+*/
+
+function replaceWords(dictionary: string[], sentence: string): string {
+  // Convert the dictionary array to a set for fast lookup
+  const rootSet = new Set(dictionary);
+  // Split the sentence into individual words
+  const words = sentence.split(' ');
+
+  // Iterate over each word in the sentence
+  for (let i = 0; i < words.length; i++) {
+    // Check each possible prefix of the word
+    for (let j = 1; j <= words[i].length; j++) {
+      // Get the current prefix
+      const prefix = words[i].substring(0, j);
+      // If the prefix is in the root set, replace the word with the prefix
+      if (rootSet.has(prefix)) {
+        words[i] = prefix;
+        break; // Exit the loop once the word is replaced
+      }
+    }
+  }
+
+  // Join the words back into a sentence and return it
+  return words.join(' ');
+}
