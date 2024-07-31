71ee73dda32fdd9a4d5d22471e43bdc39527428c
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-07-30 22:22:43 -0600

    💡 [ADD]: 1105 Filling Bookcase Shelves
    Implemented minHeightShelves function

--- problems/medium.ts
+++ problems/medium.ts
@@ -8075,3 +8075,65 @@ function minimumDeletions(s: string): number {
 
   return deletions;
 }
+
+/* 
+1105. Filling Bookcase Shelves
+
+You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
+
+We want to place these books in order onto bookcase shelves that have a total width shelfWidth.
+
+We choose some of the books to place on this shelf such that the sum of their thickness is less than or equal to shelfWidth, then build another level of the shelf of the bookcase so that the total height of the bookcase has increased by the maximum height of the books we just put down. We repeat this process until there are no more books to place.
+
+Note that at each step of the above process, the order of the books we place is the same order as the given sequence of books.
+
+For example, if we have an ordered list of 5 books, we might place the first and second book onto the first shelf, the third book on the second shelf, and the fourth and fifth book on the last shelf.
+Return the minimum possible height that the total bookshelf can be after placing shelves in this manner.
+
+Example 1:
+Input: books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4
+Output: 6
+Explanation:
+The sum of the heights of the 3 shelves is 1 + 3 + 2 = 6.
+Notice that book number 2 does not have to be on the first shelf.
+
+Example 2:
+Input: books = [[1,3],[2,4],[3,2]], shelfWidth = 6
+Output: 4
+
+Constraints:
+1 <= books.length <= 1000
+1 <= thicknessi <= shelfWidth <= 1000
+1 <= heighti <= 1000
+
+</> Typescript Code:
+*/
+
+function minHeightShelves(books: number[][], shelfWidth: number): number {
+  // Number of books
+  const n = books.length;
+  // Array to store minimum heights
+  const dp = new Array(n + 1).fill(0);
+
+  // Loop through each book
+  for (let i = 1; i <= n; i++) {
+    // Initialize variables for current book's width and height
+    let width = books[i - 1][0];
+    let height = books[i - 1][1];
+    // Initially, set dp[i] to the height of placing only this book on a new shelf
+    dp[i] = dp[i - 1] + height;
+
+    // Try placing current book with previous books to minimize shelf height
+    for (let j = i - 1; j > 0 && width + books[j - 1][0] <= shelfWidth; j--) {
+      // Update the maximum height encountered in this configuration
+      height = Math.max(height, books[j - 1][1]);
+      // Accumulate width of books being placed on the current shelf
+      width += books[j - 1][0];
+      // Update dp[i] to the minimum height of the current configuration
+      dp[i] = Math.min(dp[i], dp[j - 1] + height);
+    }
+  }
+
+  // Return the minimum possible height of the bookshelf
+  return dp[n];
+}
