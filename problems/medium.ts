4721f6a398ff68915f1b0145a1c0366d16354c4b
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2024-10-14 21:06:52 -0600

    💡 [ADD]: Solve 2938. Separate Black and White Balls
    Added a performant solution to the problem 2938. The solution efficiently counts the minimum number of swaps required to group all black balls (1s) on the right side by accumulating the necessary steps while iterating through the string.

--- problems/medium.ts
+++ problems/medium.ts
@@ -11723,3 +11723,65 @@ function maxKelements(nums: number[], k: number): number {
   }
   return score; // Return the maximum possible score after k operations
 }
+
+/* 
+2938. Separate Black and White Balls
+
+There are n balls on a table, each ball has a color black or white.
+
+You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+In each step, you can choose two adjacent balls and swap them.
+
+Return the minimum number of steps to group all the black balls to the right and all the white balls to the left.
+
+Example 1:
+Input: s = "101"
+Output: 1
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "011".
+Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
+
+Example 2:
+Input: s = "100"
+Output: 2
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "010".
+- Swap s[1] and s[2], s = "001".
+It can be proven that the minimum number of steps needed is 2.
+
+Example 3:
+Input: s = "0111"
+Output: 0
+Explanation: All the black balls are already grouped to the right.
+
+Constraints:
+1 <= n == s.length <= 10^5
+s[i] is either '0' or '1'.
+
+</> Typescript Code:
+*/
+
+function minimumSteps(s: string): number {
+  // Initialize a counter for the number of black balls encountered (1s)
+  let blackCount = 0;
+
+  // Initialize the minimum number of steps to 0
+  let minSteps = 0;
+
+  // Loop through the binary string s
+  for (let i = 0; i < s.length; i++) {
+    // If the current ball is black (1)
+    if (s[i] === '1') {
+      // Increment the black ball count
+      blackCount++;
+    } else {
+      // If the current ball is white (0), accumulate steps by adding the
+      // number of black balls encountered so far since they need to be moved
+      minSteps += blackCount;
+    }
+  }
+
+  // Return the total minimum steps needed to group all black balls on the right
+  return minSteps;
+}
