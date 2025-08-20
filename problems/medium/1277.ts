aed8bdf935911af0c5021be264a6a78c624610ff
thayrov <thayrovg@gmail.com>
2025-08-19 22:32:26 -0600

    feat: Implement count squares submatrices solution

---
+++ problems/medium/1277.ts
@@ -0,0 +1,60 @@
+/*
+1277. Count Square Submatrices with All Ones
+
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+Example 1:
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+
+Example 2:
+Input: matrix =
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+Constraints:
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+
+</> Typescript Code:
+*/
+
+function countSquares(matrix: number[][]): number {
+  let m = matrix.length,
+    n = matrix[0].length; // Get matrix dimensions
+  let result = 0; // Initialize result counter
+  for (let i = 0; i < m; i++) {
+    // Loop over rows
+    for (let j = 0; j < n; j++) {
+      // Loop over columns
+      if (matrix[i][j] > 0 && i > 0 && j > 0) {
+        // If cell is 1 and not on first row or column
+        // Update cell with size of largest square ending at (i, j)
+        matrix[i][j] =
+          Math.min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1]) +
+          1;
+      }
+      result += matrix[i][j]; // Accumulate total squares
+    }
+  }
+  return result; // Return total number of squares
+}
