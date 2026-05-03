45202eaa467c87a9697d90a570911f88cb566cd1
thayrov <thayrovg@gmail.com>
2026-05-02 23:31:34 -0600

    Add solution for Rotate String problem

--- problems/easy/easy.ts
+++ problems/easy/easy.ts
@@ -4348,41 +4348,6 @@ function isCircularSentence(sentence: string): boolean {
 }
 
 /* 
-796. Rotate String
-
-Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
-
-A shift on s consists of moving the leftmost character of s to the rightmost position.
-
-For example, if s = "abcde", then it will be "bcdea" after one shift.
-
-Example 1:
-Input: s = "abcde", goal = "cdeab"
-Output: true
-
-Example 2:
-Input: s = "abcde", goal = "abced"
-Output: false
-
-Constraints:
-1 <= s.length, goal.length <= 100
-s and goal consist of lowercase English letters.
-
-</> Typescript Code:
-*/
-
-function rotateString(s: string, goal: string): boolean {
-  // Check if both strings have the same length
-  if (s.length !== goal.length) {
-    return false;
-  }
-  // Concatenate string s with itself
-  const doubled = s + s;
-  // Check if goal is a substring of the doubled string
-  return doubled.includes(goal);
-}
-
-/* 
 1652. Defuse the Bomb
 
 You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
